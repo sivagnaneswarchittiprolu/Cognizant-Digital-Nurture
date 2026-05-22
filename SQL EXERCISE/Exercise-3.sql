199c8e6432823a4d9c622cda1d5df1e9b0bc12ad
Chittiprolu Venkata Amara Siva Gnaneswar <23pa1a4521@vishnu.edu.in>
2026-05-22 19:49:02 +0530

    Add query to find inactive users in last 90 days
    Retrieve users who have not registered for any events in the last 90 days.

---
+++ SQL EXERCISE/Exercise-3.sql
@@ -0,0 +1,6 @@
+--Retrieve users who have not registered for any events in the last 90 days.
+select user_id,full_name from users
+where user_id not in(
+          select user_id from registratins
+          where registration_date >= (day,90,GETDATE())
+)
