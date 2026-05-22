0713d3ed5c76a1bf5ab1bd3c36ab3225c8b8dc56
Chittiprolu Venkata Amara Siva Gnaneswar <23pa1a4521@vishnu.edu.in>
2026-05-22 19:18:55 +0530

    Update Exercise-1.sql

--- SQL EXERCISE/Exercise-1.sql
+++ SQL EXERCISE/Exercise-1.sql
@@ -1,1 +1,10 @@
-
+select
+u.full_name,
+u.city,
+e.title as event_name,
+e.start_date
+from users as u
+join Registrations as r on u.user_id = r.user_id
+join Events as e on e.event_id = r.user_id
+where e.status = 'upcoming' and u.city = e.city
+order by e.start_date
