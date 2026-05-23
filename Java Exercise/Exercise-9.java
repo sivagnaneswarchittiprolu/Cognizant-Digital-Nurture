326d55bf9aa2ec0e30179e7141696bf46f0b7ddc
Chittiprolu Venkata Amara Siva Gnaneswar <23pa1a4521@vishnu.edu.in>
2026-05-23 21:04:11 +0530

    Create Exercise-9.java

---
+++ Java Exercise/Exercise-9.java
@@ -0,0 +1,42 @@
+// 9. Grade Calculator
+// • Objective: Use conditional statements to determine grades.
+// • Task: Assign grades based on marks entered by the user.
+// • Instructions:
+// o Prompt the user for marks out of 100.
+// o Use if-else statements to assign grades:
+// ▪ 90-100: A
+// ▪ 80-89: B
+// ▪ 70-79: C
+// ▪ 60-69: D
+// ▪ Below 60: F
+// o Display the assigned grade
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks out of 100: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90 && marks <= 100) {
+            System.out.println("Grade: A");
+        } 
+        else if (marks >= 80) {
+            System.out.println("Grade: B");
+        } 
+        else if (marks >= 70) {
+            System.out.println("Grade: C");
+        } 
+        else if (marks >= 60) {
+            System.out.println("Grade: D");
+        } 
+        else {
+            System.out.println("Grade: F");
+        }
+
+        sc.close();
+    }
+}
