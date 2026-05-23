e84a2dba9fe8f981ee77249c7d8d02680d1d9c65
Chittiprolu Venkata Amara Siva Gnaneswar <23pa1a4521@vishnu.edu.in>
2026-05-23 21:40:04 +0530

    Add recursive Fibonacci calculation
    Implement recursive method to calculate Fibonacci number.

---
+++ Java Exercise/Exercise-13.java
@@ -0,0 +1,39 @@
+// 13. Recursive Fibonacci
+// • Objective: Implement recursion.
+// • Task: Calculate the nth Fibonacci number using recursion.
+// • Instructions:
+// o Prompt the user for a positive integer n.
+// o Define a recursive method fibonacci(int n) that returns the nth Fibonacci number.
+// o Display the result.
+
+
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+
+    // Recursive method to find Fibonacci number
+    static int fibonacci(int n) {
+
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer: ");
+        int n = sc.nextInt();
+
+        System.out.println("Fibonacci number at position " + n + " is " + fibonacci(n));
+
+        sc.close();
+    }
+}
