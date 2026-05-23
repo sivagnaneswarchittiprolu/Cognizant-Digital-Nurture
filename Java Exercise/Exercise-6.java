0dafb750e4de7635887c2138ce5957a08ffd5c19
Chittiprolu Venkata Amara Siva Gnaneswar <23pa1a4521@vishnu.edu.in>
2026-05-23 21:00:02 +0530

    Add DataTypeDemo to demonstrate primitive types
    This program demonstrates Java's primitive data types by declaring variables of types int, float, double, char, and boolean, and displaying their values.

---
+++ Java Exercise/Exercise-6.java
@@ -0,0 +1,25 @@
+// 6. Data Type Demonstration
+// • Objective: Understand Java's primitive data types.
+// • Task: Declare variables of different primitive types and display their values.
+// • Instructions:
+// o Declare variables of types int, float, double, char, and boolean.
+// o Assign appropriate values to each.
+// o Use System.out.println() to display each variable.
+
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+
+        int num = 100;
+        float decimal1 = 12.5f;
+        double decimal2 = 99.99;
+        char letter = 'A';
+        boolean isJavaFun = true;
+
+        System.out.println("Integer Value: " + num);
+        System.out.println("Float Value: " + decimal1);
+        System.out.println("Double Value: " + decimal2);
+        System.out.println("Character Value: " + letter);
+        System.out.println("Boolean Value: " + isJavaFun);
+    }
+}
