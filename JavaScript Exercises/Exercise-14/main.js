845d8da5846a43535b253996ae275ec482592858
Chittiprolu Venkata Amara Siva Gnaneswar <23pa1a4521@vishnu.edu.in>
2026-05-31 16:02:04 +0530

    Create main.js

---
+++ JavaScript Exercises/Exercise-14/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+    $("#registerBtn").click(function () {
+
+        alert("Successfully Registered!");
+
+    });
+
+    $("#hideBtn").click(function () {
+
+        $("#eventCard").fadeOut();
+
+    });
+
+    $("#showBtn").click(function () {
+
+        $("#eventCard").fadeIn();
+
+    });
+
+});
